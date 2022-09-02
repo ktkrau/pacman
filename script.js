a0edb670b35c71462da254cfdd9489f6dd724d5e
ktkrau <ktkrau@gmail.com>
2022-09-02 16:14:03 -0400

    agregue movimientos con teclas, falta arreglar movimiento de paredes

--- script.js
+++ script.js
@@ -11,23 +11,52 @@ var world = [
     [2,2,2,2,2,2,2,2,2,2]
 
 ];
+var pacman = {
+    x: 0,
+    y: 0
+}
 
 function displayWorld(){
     var output = '';
 
     for(var i=0;i<world.length;i++){
-        output += "<div class='row'>"
+        output += "\n<div class='row'>\n"
         for(var j = 0; j<world[i].length; j++){
             if(world[i][j] == 2)
-                output += "\n\t<div class='brick'></div>";
+                output += "<div class='brick'></div>";
             else if(world[i][j] == 1)
-                output += "\n\t<div class='coin'></div>";
+                output += "<div class='coin'></div>";
                 if(world[i][j] == 0)
-                output += "\n\t<div class='empty'></div>";
+                output += "\n<div class='empty'></div>";
 
             }
-            output += "\n</div>"
+            output += "</div>"
     }
     console.log(output);
     document.getElementById('world').innerHTML = output;
+}
+
+function displayPacman(){
+    document.getElementById('pacman').style.top = pacman.y*20+"px";
+    document.getElementById('pacman').style.left = pacman.x*20+"px"
+}
+displayWorld();
+displayPacman();
+
+document.onkeydown = function(e){
+    if(e.keyCode == 37){
+        pacman.x --;
+    }else if(e.keyCode == 39){
+        pacman.x ++;
+    }else if(e.keyCode == 38){
+        pacman.y --;
+    }else if(e.keyCode == 40){
+        pacman.y ++;
+    }
+    if(world[pacman.y][pacman.x] == 1){
+        world[pacman.y][pacman.x] = 0
+        displayWorld(); 
+        console.log(displayWorld())
+    }
+    displayPacman();
 }
